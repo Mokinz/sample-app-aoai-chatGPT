a4c2d0799c8d4babee68c68140fe4eaa8819fa61
Mokinz <jz.jakubzolich@gmail.com>
2024-06-18 10:40:10 +0200

    SMALL CHANGES

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "ANEGIS"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Zacznij pisać"
+    chat_description: str = "Ten tekst został zmieniony"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
 
